7c0ed73e84cdd48984fe2030e977005267e3f8cd
mogulili <1244076248@qq.com>
2018-06-01 17:29:51 +0800

    2018-6-1

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var listcon = [
+    // {
+    //     title:'列表一'
+    // },
+    // {
+    //     titel:'列表二'
+    // }
+];
+new Vue({
+    el:"#main",
+    data:{
+        listcon:listcon
+    }
+})
